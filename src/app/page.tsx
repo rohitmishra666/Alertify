2a73ce456f5b194674140b3e57f2646bf8725011
Rohit Mishra <rohitmishra.14rm@gmail.com>
2024-07-28 01:19:37 +0530

    edit in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,11 @@ export default function Home() {
   const [notificationPermission, setNotificationPermission] = useState<NotificationPermission | null>(null);
 
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
-        });
-    }
-  }, []);
-
-  useEffect(() => {
     if (typeof window !== 'undefined' && 'Notification' in window) {
       setNotificationPermission(Notification.permission);
+    }
+    if(notificationPermission === 'default'){
+      requestNotificationPermission()
     }
   }, []);
 
